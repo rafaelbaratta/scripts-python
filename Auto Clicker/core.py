ec07963673efe26abcf7adb6a4b00f9bdbab44a4
Rafael Vinicius Baratta <rafaelbaratta@hotmail.com>
2026-02-08 20:47:48 -0300

    Auto Clicker - Melhoria da interface e adição de pequenas melhorias e funcionalidades

--- Auto Clicker/core.py
+++ Auto Clicker/core.py
@@ -1,3 +1,4 @@
+import re
 import threading
 from time import sleep
 
@@ -11,6 +12,29 @@ class Core:
     def __init__(self, gui):
         self.gui = gui
 
+        self.configure_variables()
+        self.configure_buttons()
+        self.print_initial_slider_positions()
+        self.key_listener_thread()
+
+    # ========== STARTING APP FUNCTIONS ==========
+
+    def configure_variables(self):
+        self.f_keys = [
+            keyboard.Key.f1,
+            keyboard.Key.f2,
+            keyboard.Key.f3,
+            keyboard.Key.f4,
+            keyboard.Key.f5,
+            keyboard.Key.f6,
+            keyboard.Key.f7,
+            keyboard.Key.f8,
+            keyboard.Key.f9,
+            keyboard.Key.f10,
+            keyboard.Key.f11,
+            keyboard.Key.f12,
+        ]
+
         self.listener = None
 
         self.clicking = False
@@ -21,12 +45,13 @@ class Core:
         self.positions = []
         self.clicks_counter = 0
 
-        self.position_key = keyboard.Key.f1
-        self.clicker_key = keyboard.Key.f2
-        self.fixed_clicker_key = keyboard.Key.f3
+        self.position_key = self.f_keys[0]
+        self.clicker_key = self.f_keys[1]
+        self.fixed_clicker_key = self.f_keys[2]
 
-        self.gui.positions_start.configure(command=self.record_positions)
-        self.gui.positions_end.configure(command=lambda: self.end_record_positions("click"))
+    def configure_buttons(self):
+        self.gui.positions_start.configure(command=self.start_recording_positions)
+        self.gui.positions_end.configure(command=lambda: self.finish_recording_positions("click"))
         self.gui.reset_positions.configure(command=self.reset_positions)
 
         self.gui.start_button.configure(command=self.start_clicker)
@@ -36,11 +61,18 @@ class Core:
         self.gui.fixed_clicker_start_button.configure(command=self.start_fixed_clicker)
         self.gui.fixed_clicker_finish_button.configure(command=self.finish_fixed_clicker)
 
-        self.gui.speed_slider.configure(command=self.print_speed_value)
+        self.gui.clicks_delay_slider.configure(command=self.print_clicker_delay_values)
+        self.gui.cycles_delay_slider.configure(command=self.print_cycles_delay_values)
+        self.gui.fixed_clicks_delay_slider.configure(command=self.print_fixed_clicker_delay_values)
+
+        self.gui.apply_button.configure(command=self.verify_shortcut_keys)
 
         self.gui.protocol("WM_DELETE_WINDOW", self.cleanup)
 
-        self.key_listener_thread()
+    def print_initial_slider_positions(self):
+        self.print_clicker_delay_values(self.gui.clicks_delay_slider.get())
+        self.print_cycles_delay_values(self.gui.cycles_delay_slider.get())
+        self.print_fixed_clicker_delay_values(self.gui.fixed_clicks_delay_slider.get())
 
     def key_listener_thread(self):
         key_listener_thread = threading.Thread(target=lambda: keyboard.Listener(on_press=self.on_press).start())
@@ -50,9 +82,9 @@ class Core:
     def on_press(self, key):
         if key == self.position_key:
             if self.recording:
-                self.end_record_positions("keyboard")
+                self.finish_recording_positions("keyboard")
             else:
-                self.record_positions()
+                self.start_recording_positions()
         elif key == self.clicker_key:
             if self.clicking:
                 self.finish_clicker()
@@ -64,22 +96,22 @@ class Core:
             else:
                 self.start_fixed_clicker()
 
-    def record_positions(self):
+    # ========== POSITIONS SECTION FUNCTIONS ==========
+
+    def start_recording_positions(self):
         if self.clicking or self.fixed_clicking:
             return
 
+        self.reset_positions()
         self.recording = True
 
         self.gui.positions_start.pack_forget()
         self.gui.positions_end.pack(side="left", padx=(70, 0))
 
-        self.positions = []
-        self.print_positions()
-
-        self.listener = mouse.Listener(on_click=self.on_click)
+        self.listener = mouse.Listener(on_click=self.record_position_on_click)
         self.listener.start()
 
-    def end_record_positions(self, event):
+    def finish_recording_positions(self, event):
         if self.clicking or self.fixed_clicking:
             return
 
@@ -92,22 +124,41 @@ class Core:
 
         if event == "click" and self.positions:
             self.positions.pop()
-            self.print_positions()
+            self.count_positions()
 
-    def reset_positions(self):
-        self.positions = []
-        self.print_positions()
-
-    def on_click(self, x, y, button, pressed):
-
-        button_side = "left" if "left" in str(button) else "right"
+    def record_position_on_click(self, x, y, button, pressed):
+        button = str(button).split(".")[1]
 
         if pressed:
-            self.positions.append((button_side, x, y))
-            self.print_positions()
+            self.positions.append((button, x, y))
+            self.count_positions()
 
-    def print_positions(self):
+    def count_positions(self):
         self.gui.positions_counter.configure(text=f"Total de posições: {len(self.positions)}")
+
+    def reset_positions(self):
+        if self.clicking or self.fixed_clicking:
+            return
+
+        self.positions = []
+        self.count_positions()
+
+    # ========== STANDARD FUNCTIONS FOR BOTH CLICKERS ==========
+
+    def start_clicker_thread(self, target):
+        self.clicker_thread = threading.Thread(target=target)
+        self.clicker_thread.daemon = True
+        self.clicker_thread.start()
+
+    def increase_clicks(self):
+        self.clicks_counter += 1
+        self.gui.clicks_counter.configure(text=f"Total de cliques: {self.clicks_counter}")
+
+    def reset_clicks(self):
+        self.clicks_counter = 0
+        self.gui.clicks_counter.configure(text=f"Total de cliques: {self.clicks_counter}")
+
+    # ========== CLICKER SECTION FUNCTIONS ==========
 
     def start_clicker(self):
         if self.recording or self.fixed_clicking:
@@ -122,14 +173,15 @@ class Core:
             return
 
         self.clicking = True
-        self.double_click = self.gui.double_click_checkbox.get()
+        double_click = self.gui.double_click_checkbox.get()
 
         self.gui.start_button.pack_forget()
         self.gui.finish_button.pack(side="left", padx=(70, 0))
 
-        clicker_thread = threading.Thread(target=self.clicker)
-        clicker_thread.daemon = True
-        clicker_thread.start()
+        if double_click:
+            self.start_clicker_thread(self.double_clicker)
+        else:
+            self.start_clicker_thread(self.clicker)
 
     def finish_clicker(self):
         if self.recording or self.fixed_clicking:
@@ -141,60 +193,54 @@ class Core:
         self.gui.finish_button.pack_forget()
 
     def clicker(self):
-        delay = self.gui.speed_slider.get()
+        clicks_delay = self.gui.clicks_delay_slider.get()
+        cycles_delay = self.gui.cycles_delay_slider.get()
 
-        if self.double_click:
-            while self.clicking:
-                for button, x, y in self.positions:
-                    if not self.clicking:
-                        break
-                    doubleClick(x, y, button=button)
-                    self.increase_clicks()
-                    self.increase_clicks()
-                    sleep(delay)
-        else:
-            while self.clicking:
-                for button, x, y in self.positions:
-                    if not self.clicking:
-                        break
-                    click(x, y, button=button)
-                    self.increase_clicks()
-                    sleep(delay)
+        while self.clicking:
+            for button, x, y in self.positions:
+                if not self.clicking:
+                    break
+                click(x, y, button=button)
+                self.increase_clicks()
+                sleep(clicks_delay)
+            sleep(cycles_delay)
 
-    def increase_clicks(self):
-        self.clicks_counter += 1
-        self.gui.clicks_counter.configure(text=f"Total de cliques: {self.clicks_counter}")
+    def double_clicker(self):
+        clicks_delay = self.gui.clicks_delay_slider.get()
+        cycles_delay = self.gui.cycles_delay_slider.get()
 
-    def reset_clicks(self):
-        self.clicks_counter = 0
-        self.gui.clicks_counter.configure(text=f"Total de cliques: {self.clicks_counter}")
+        while self.clicking:
+            for button, x, y in self.positions:
+                if not self.clicking:
+                    break
+                doubleClick(x, y, button=button)
+                self.increase_clicks()
+                self.increase_clicks()
+                sleep(clicks_delay)
+            sleep(cycles_delay)
 
-    def print_speed_value(self, value):
-        self.gui.speed_value.configure(text=f"Velocidade dos cliques: ({value:.3f})")
+    def print_clicker_delay_values(self, value):
+        self.gui.clicks_delay_value.configure(text=f"Intervalo cliques: ({value:.3f})")
 
-    def cleanup(self):
-        self.clicking = False
-        self.fixed_clicking = False
-        self.recording = False
+    def print_cycles_delay_values(self, value):
+        self.gui.cycles_delay_value.configure(text=f"Intervalo ciclos: ({value:.3f})")
 
-        if self.listener:
-            self.listener.stop()
-
-        self.gui.destroy()
+    # ========== FIXED CLICKER SECTION FUNCTIONS ==========
 
     def start_fixed_clicker(self):
         if self.recording or self.clicking:
             return
 
         self.fixed_clicking = True
-        self.double_click = self.gui.double_click_checkbox.get()
+        double_click = self.gui.fixed_double_click_checkbox.get()
 
         self.gui.fixed_clicker_start_button.pack_forget()
         self.gui.fixed_clicker_finish_button.pack(pady=5)
 
-        clicker_thread = threading.Thread(target=self.fixed_clicker)
-        clicker_thread.daemon = True
-        clicker_thread.start()
+        if double_click:
+            self.start_clicker_thread(self.fixed_double_clicker)
+        else:
+            self.start_clicker_thread(self.fixed_clicker)
 
     def finish_fixed_clicker(self):
         if self.recording or self.clicking:
@@ -206,16 +252,110 @@ class Core:
         self.gui.fixed_clicker_start_button.pack(pady=5)
 
     def fixed_clicker(self):
-        delay = self.gui.speed_slider.get()
+        delay = self.gui.fixed_clicks_delay_slider.get()
 
-        if self.double_click:
-            while self.fixed_clicking:
-                doubleClick(button="left")
-                self.increase_clicks()
-                self.increase_clicks()
-                sleep(delay)
+        while self.fixed_clicking:
+            click(button="left")
+            self.increase_clicks()
+            sleep(delay)
+
+    def fixed_double_clicker(self):
+        delay = self.gui.fixed_clicks_delay_slider.get()
+
+        while self.fixed_clicking:
+            doubleClick(button="left")
+            self.increase_clicks()
+            self.increase_clicks()
+            sleep(delay)
+
+    def print_fixed_clicker_delay_values(self, value):
+        self.gui.fixed_clicks_delay_value.configure(text=f"Intervalo cliques: ({value:.3f})")
+
+    # ========== SHORTCUT KEYS SECTION FUNCTIONS ==========
+
+    def verify_shortcut_keys(self):
+        self.verify_position_shortcut_key()
+        self.verify_clicker_shortcut_key()
+        self.verify_fixed_clicker_shortcut_key()
+
+    def verify_position_shortcut_key(self):
+        value = self.gui.position_key_entry.get()
+
+        if not value or not self.verify_valid_value(value):
+            return
+
+        key = int(value[1:]) - 1
+        f_key = self.f_keys[key]
+        if self.clicker_key == f_key or self.fixed_clicker_key == f_key:
+            CTkMessagebox(
+                title="Info!",
+                message="O atalho para o controle das posições não foi alterado por conflitar com outro atalho",
+                icon="info",
+            )
+            return False
+        self.position_key = f_key
+        self.gui.position_key_entry.delete(0, "end")
+        self.gui.position_key_entry.configure(placeholder_text=value.upper())
+
+    def verify_clicker_shortcut_key(self):
+        value = self.gui.clicker_key_entry.get()
+
+        if not value or not self.verify_valid_value(value):
+            return
+
+        key = int(value[1:]) - 1
+        f_key = self.f_keys[key]
+        if self.position_key == f_key or self.fixed_clicker_key == f_key:
+            CTkMessagebox(
+                title="Info!",
+                message="O atalho para o controle do clicker não foi alterado por conflitar com outro atalho",
+                icon="info",
+            )
+            return False
+        self.clicker_key = f_key
+        self.gui.clicker_key_entry.delete(0, "end")
+        self.gui.clicker_key_entry.configure(placeholder_text=value.upper())
+
+    def verify_fixed_clicker_shortcut_key(self):
+        value = self.gui.fixed_clicker_key_entry.get()
+
+        if not value or not self.verify_valid_value(value):
+            return
+
+        key = int(value[1:]) - 1
+        f_key = self.f_keys[key]
+        if self.position_key == f_key or self.clicker_key == f_key:
+            CTkMessagebox(
+                title="Info!",
+                message="O atalho para o controle do clicker fixo não foi alterado por conflitar com outro atalho",
+                icon="info",
+            )
+            return False
+        self.fixed_clicker_key = f_key
+        self.gui.fixed_clicker_key_entry.delete(0, "end")
+        self.gui.fixed_clicker_key_entry.configure(placeholder_text=value.upper())
+
+    def verify_valid_value(self, value):
+        value = value.strip().upper()
+
+        if re.match("^F(1[0-2]|[1-9])$", value):
+            return True
         else:
-            while self.fixed_clicking:
-                click(button="left")
-                self.increase_clicks()
-                sleep(delay)
+            CTkMessagebox(
+                title="Info!",
+                message="Valores possíveis:\nF1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12",
+                icon="info",
+            )
+            return False
+
+    # ========== FINISHING APP FUNCTIONS ==========
+
+    def cleanup(self):
+        self.clicking = False
+        self.fixed_clicking = False
+        self.recording = False
+
+        if self.listener:
+            self.listener.stop()
+
+        self.gui.destroy()
--- Auto Clicker/gui.py
+++ Auto Clicker/gui.py
@@ -18,43 +18,36 @@ class Gui(ctk.CTk):
         }
 
         self.FONTS = {
-            "title": ("Arial", 28, "bold"),
             "text": ("Arial", 14),
             "message": ("Arial", 12),
             "button": ("Arial", 16, "bold"),
         }
 
         self.height = 40
+        self.pady = 5
 
         self.build()
 
     def build(self):
         self.title("Auto Clicker")
-        self.geometry("500x580")
+        self.geometry("500x620")
         self.resizable(False, False)
 
-        title_container = ctk.CTkFrame(self, fg_color=self.COLORS["secondary_bg"])
-        title_container.pack(fill="x", padx=10, pady=10)
+        self.build_positions_section()
+        self.build_clicker_section()
+        self.build_fixed_clicker_section()
+        self.build_shortcut_keys_section()
 
-        ctk.CTkLabel(
-            title_container,
-            text="Auto Clicker",
-            font=self.FONTS["title"],
-            text_color=self.COLORS["text"],
-        ).pack(pady=15)
-
-        content_container = ctk.CTkFrame(self, fg_color=self.COLORS["primary_bg"])
-        content_container.pack(fill="both")
-
-        positions_container = ctk.CTkFrame(content_container, fg_color=self.COLORS["secondary_bg"])
-        positions_container.pack(fill="x", pady=5, padx=10)
+    def build_positions_section(self):
+        positions_container = ctk.CTkFrame(self, fg_color=self.COLORS["secondary_bg"])
+        positions_container.pack(fill="x", pady=self.pady, padx=10)
 
         ctk.CTkLabel(
             positions_container,
             text="Pressione o botão para gravar as posições dos cliques",
             font=self.FONTS["text"],
             text_color=self.COLORS["text"],
-        ).pack(pady=5, padx=30)
+        ).pack(pady=self.pady, padx=30)
 
         self.positions_counter = ctk.CTkLabel(
             positions_container,
@@ -64,10 +57,10 @@ class Gui(ctk.CTk):
             text_color=self.COLORS["text"],
             fg_color=self.COLORS["primary_bg"],
         )
-        self.positions_counter.pack(pady=5, padx=30)
+        self.positions_counter.pack(pady=self.pady, padx=30)
 
         positions_buttons_frame = ctk.CTkFrame(positions_container, fg_color="transparent")
-        positions_buttons_frame.pack(fill="x", pady=5, padx=10)
+        positions_buttons_frame.pack(fill="x", pady=self.pady, padx=10)
 
         self.positions_start = ctk.CTkButton(
             positions_buttons_frame,
@@ -101,22 +94,16 @@ class Gui(ctk.CTk):
         )
         self.reset_positions.pack(side="right", padx=(0, 70))
 
-        ctk.CTkLabel(
-            positions_container,
-            text="(As posições podem ser controladas pela tecla 'F1')",
-            font=self.FONTS["message"],
-            text_color=self.COLORS["text"],
-        ).pack(pady=5, padx=30)
-
-        clicker_container = ctk.CTkFrame(content_container, fg_color=self.COLORS["secondary_bg"])
-        clicker_container.pack(fill="x", pady=5, padx=10)
+    def build_clicker_section(self):
+        clicker_container = ctk.CTkFrame(self, fg_color=self.COLORS["secondary_bg"])
+        clicker_container.pack(fill="x", pady=self.pady, padx=10)
 
         ctk.CTkLabel(
             clicker_container,
-            text="Clique nos botões para controlar o 'Auto Clicker'",
+            text="Controles para o 'Auto Clicker' nas posições gravadas",
             font=self.FONTS["text"],
             text_color=self.COLORS["text"],
-        ).pack(pady=5, padx=30)
+        ).pack(pady=self.pady, padx=30)
 
         self.clicks_counter = ctk.CTkLabel(
             clicker_container,
@@ -126,25 +113,40 @@ class Gui(ctk.CTk):
             text_color=self.COLORS["text"],
             fg_color=self.COLORS["primary_bg"],
         )
-        self.clicks_counter.pack(pady=5, padx=30)
+        self.clicks_counter.pack(pady=self.pady, padx=30)
 
-        clicker_speed_frame = ctk.CTkFrame(clicker_container, fg_color="transparent")
-        clicker_speed_frame.pack(fill="x", pady=5, padx=10)
+        options_container = ctk.CTkFrame(clicker_container, fg_color="transparent")
+        options_container.pack(fill="x")
 
-        self.speed_value = ctk.CTkLabel(
-            clicker_speed_frame,
-            text="Velocidade dos cliques: (5.000)",
+        for i in range(3):
+            options_container.grid_columnconfigure(i, weight=1)
+
+        self.clicks_delay_value = ctk.CTkLabel(
+            options_container,
+            text="Intervalo cliques:",
             font=self.FONTS["text"],
             text_color=self.COLORS["text"],
         )
-        self.speed_value.pack(side="left", padx=(30, 0))
+        self.clicks_delay_value.grid(row=0, column=0, pady=self.pady)
 
-        self.speed_slider = ctk.CTkSlider(clicker_speed_frame, from_=0.005, to=10.0)
-        self.speed_slider.pack(fill="x", side="right", padx=(0, 30))
-        self.speed_slider.set(5)
+        self.clicks_delay_slider = ctk.CTkSlider(options_container, width=150, from_=0.001, to=120.0)
+        self.clicks_delay_slider.grid(row=0, column=1, pady=self.pady)
+        self.clicks_delay_slider.set(5)
+
+        self.cycles_delay_value = ctk.CTkLabel(
+            options_container,
+            text="Intervalo ciclos:",
+            font=self.FONTS["text"],
+            text_color=self.COLORS["text"],
+        )
+        self.cycles_delay_value.grid(row=1, column=0, pady=self.pady)
+
+        self.cycles_delay_slider = ctk.CTkSlider(options_container, width=150, from_=0.001, to=120.0)
+        self.cycles_delay_slider.grid(row=1, column=1, pady=self.pady)
+        self.cycles_delay_slider.set(5)
 
         self.double_click_checkbox = ctk.CTkCheckBox(
-            clicker_container,
+            options_container,
             checkbox_width=20,
             checkbox_height=20,
             border_width=2,
@@ -154,10 +156,10 @@ class Gui(ctk.CTk):
             fg_color=self.COLORS["button"],
             hover_color=self.COLORS["button_hover"],
         )
-        self.double_click_checkbox.pack()
+        self.double_click_checkbox.grid(row=0, column=2, rowspan=2)
 
         clicker_buttons_frame = ctk.CTkFrame(clicker_container, fg_color="transparent")
-        clicker_buttons_frame.pack(fill="x", pady=5, padx=10)
+        clicker_buttons_frame.pack(fill="x", pady=self.pady, padx=10)
 
         self.start_button = ctk.CTkButton(
             clicker_buttons_frame,
@@ -191,40 +193,147 @@ class Gui(ctk.CTk):
         )
         self.reset_clicks.pack(side="right", padx=(0, 70))
 
-        ctk.CTkLabel(
-            clicker_container,
-            text="(O Clicker pode ser controlado pela tecla 'F2')",
-            font=self.FONTS["message"],
-            text_color=self.COLORS["text"],
-        ).pack(pady=5, padx=30)
+    def build_fixed_clicker_section(self):
+        fixed_clicker_container = ctk.CTkFrame(self, fg_color=self.COLORS["secondary_bg"])
+        fixed_clicker_container.pack(fill="x", pady=self.pady, padx=10)
 
-        fixed_clicker_buttons_frame = ctk.CTkFrame(clicker_container, fg_color="transparent")
+        ctk.CTkLabel(
+            fixed_clicker_container,
+            text="Controles para o 'Auto Clicker' fixo na posição do mouse",
+            font=self.FONTS["text"],
+            text_color=self.COLORS["text"],
+        ).pack(pady=self.pady, padx=30)
+
+        options_container = ctk.CTkFrame(fixed_clicker_container, fg_color="transparent")
+        options_container.pack(fill="x")
+
+        for i in range(3):
+            options_container.grid_columnconfigure(i, weight=1)
+
+        self.fixed_clicks_delay_value = ctk.CTkLabel(
+            options_container,
+            text="Intervalo cliques:",
+            font=self.FONTS["text"],
+            text_color=self.COLORS["text"],
+        )
+        self.fixed_clicks_delay_value.grid(row=0, column=0, pady=self.pady)
+
+        self.fixed_clicks_delay_slider = ctk.CTkSlider(options_container, width=150, from_=0.001, to=120.0)
+        self.fixed_clicks_delay_slider.grid(row=0, column=1, pady=self.pady)
+        self.fixed_clicks_delay_slider.set(5)
+
+        self.fixed_double_click_checkbox = ctk.CTkCheckBox(
+            options_container,
+            checkbox_width=20,
+            checkbox_height=20,
+            border_width=2,
+            text="Duplo Clique?",
+            font=self.FONTS["text"],
+            text_color=self.COLORS["text"],
+            fg_color=self.COLORS["button"],
+            hover_color=self.COLORS["button_hover"],
+        )
+        self.fixed_double_click_checkbox.grid(row=0, column=2)
+
+        fixed_clicker_buttons_frame = ctk.CTkFrame(fixed_clicker_container, fg_color="transparent")
         fixed_clicker_buttons_frame.pack(fill="x", padx=10)
 
         self.fixed_clicker_start_button = ctk.CTkButton(
             fixed_clicker_buttons_frame,
             height=self.height,
-            text="Iniciar Clicker Fixo na Posição do Mouse",
+            text="Iniciar Clicker",
             font=self.FONTS["button"],
             text_color=self.COLORS["button_text"],
             fg_color=self.COLORS["button"],
             hover_color=self.COLORS["button_hover"],
         )
-        self.fixed_clicker_start_button.pack(pady=5)
+        self.fixed_clicker_start_button.pack(pady=self.pady)
 
         self.fixed_clicker_finish_button = ctk.CTkButton(
             fixed_clicker_buttons_frame,
             height=self.height,
-            text="Encerrar Clicker Fixo na Posição do Mouse",
+            text="Encerrar Clicker",
             font=self.FONTS["button"],
             text_color=self.COLORS["button_text"],
             fg_color=self.COLORS["button"],
             hover_color=self.COLORS["button_hover"],
         )
 
+    def build_shortcut_keys_section(self):
+        shortcut_keys_container = ctk.CTkFrame(self, fg_color=self.COLORS["secondary_bg"])
+        shortcut_keys_container.pack(fill="x", pady=self.pady, padx=10)
+
+        for i in range(6):
+            shortcut_keys_container.grid_columnconfigure(i, weight=1)
+
         ctk.CTkLabel(
-            clicker_container,
-            text="(O Clicker fixo pode ser controlado pela tecla 'F3')",
-            font=self.FONTS["message"],
+            shortcut_keys_container,
+            text="Defina os atalhos para controlar o aplicativo",
+            font=self.FONTS["text"],
             text_color=self.COLORS["text"],
-        ).pack(pady=5, padx=30)
+        ).grid(row=0, column=0, columnspan=6, pady=self.pady, padx=30, sticky="nsew")
+
+        ctk.CTkLabel(
+            shortcut_keys_container,
+            text="Posições",
+            font=self.FONTS["text"],
+            text_color=self.COLORS["text"],
+        ).grid(row=1, column=0, pady=self.pady, padx=15)
+
+        self.position_key_entry = ctk.CTkEntry(
+            shortcut_keys_container,
+            height=30,
+            width=40,
+            font=self.FONTS["text"],
+            text_color=self.COLORS["text"],
+            placeholder_text="F1",
+            justify="center",
+        )
+        self.position_key_entry.grid(row=1, column=1, pady=self.pady, padx=15, sticky="nsew")
+
+        ctk.CTkLabel(
+            shortcut_keys_container,
+            text="Clicker",
+            font=self.FONTS["text"],
+            text_color=self.COLORS["text"],
+        ).grid(row=1, column=2, pady=self.pady, padx=15, sticky="nsew")
+
+        self.clicker_key_entry = ctk.CTkEntry(
+            shortcut_keys_container,
+            height=30,
+            width=40,
+            font=self.FONTS["text"],
+            text_color=self.COLORS["text"],
+            placeholder_text="F2",
+            justify="center",
+        )
+        self.clicker_key_entry.grid(row=1, column=3, pady=self.pady, padx=15, sticky="nsew")
+
+        ctk.CTkLabel(
+            shortcut_keys_container,
+            text="Clicker Fixo",
+            font=self.FONTS["text"],
+            text_color=self.COLORS["text"],
+        ).grid(row=1, column=4, pady=self.pady, padx=15, sticky="nsew")
+
+        self.fixed_clicker_key_entry = ctk.CTkEntry(
+            shortcut_keys_container,
+            height=30,
+            width=40,
+            font=self.FONTS["text"],
+            text_color=self.COLORS["text"],
+            placeholder_text="F3",
+            justify="center",
+        )
+        self.fixed_clicker_key_entry.grid(row=1, column=5, pady=self.pady, padx=15, sticky="nsew")
+
+        self.apply_button = ctk.CTkButton(
+            shortcut_keys_container,
+            height=self.height,
+            text="Aplicar Atalhos",
+            font=self.FONTS["button"],
+            text_color=self.COLORS["button_text"],
+            fg_color=self.COLORS["button"],
+            hover_color=self.COLORS["button_hover"],
+        )
+        self.apply_button.grid(row=2, column=0, columnspan=6, pady=self.pady)
